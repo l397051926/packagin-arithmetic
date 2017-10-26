a7cc0c683401df5f9009fefb0df09563df536e9f
chenjinfeng <chenjinfeng@gennlife.com>
2017-10-26 20:59:15 +0800

    GZipUtils

---
+++ src/main/java/com/gennlife/packagingservice/arithmetic/utils/GZIPUtils.java
@@ -0,0 +1,78 @@
+package com.gennlife.packagingservice.arithmetic.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ * Created by Chenjinfeng on 2017/10/26.
+ */
+public class GZIPUtils {
+    private static final int BUFFER = 10240;
+    private static final Logger logger= LoggerFactory.getLogger(GZIPUtils.class);
+    public static byte[] decompress(byte[] bytes) {
+        if (bytes == null || bytes.length == 0) {
+            return null;
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+        try {
+            GZIPInputStream ungzip = new GZIPInputStream(in);
+            byte[] buffer = new byte[BUFFER];
+            int n;
+            while ((n = ungzip.read(buffer)) >= 0) {
+                out.write(buffer, 0, n);
+            }
+        } catch (IOException e) {
+            logger.error("gzip uncompress error.", e);
+        }
+
+        return out.toByteArray();
+    }
+
+    public static byte[] compress(String str, String encoding) {
+        if (str == null || str.length() == 0) {
+            return null;
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        GZIPOutputStream gzip;
+        try {
+            gzip = new GZIPOutputStream(out);
+            gzip.write(str.getBytes(encoding));
+            gzip.close();
+        } catch (IOException e) {
+            logger.error("gzip compress error.", e);
+        }
+        return out.toByteArray();
+    }
+    public static void decompress(InputStream is, OutputStream os)
+            throws Exception {
+
+        GZIPInputStream gis = new GZIPInputStream(is);
+
+        int count;
+        byte data[] = new byte[BUFFER];
+        while ((count = gis.read(data, 0, BUFFER)) != -1) {
+            os.write(data, 0, count);
+        }
+
+        gis.close();
+    }
+
+    public static void compress(InputStream is, OutputStream os)
+            throws Exception {
+
+        GZIPOutputStream gos = new GZIPOutputStream(os);
+        int count;
+        byte data[] = new byte[BUFFER];
+        while ((count = is.read(data, 0, BUFFER)) != -1) {
+            gos.write(data, 0, count);
+        }
+        gos.finish();
+        gos.flush();
+        gos.close();
+    }
+}
